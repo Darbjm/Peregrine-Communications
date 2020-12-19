817f830c0ddc4be7394136f8159f412c812189fe
James Darby <56593075+Darbjm@users.noreply.github.com>
2020-12-19 23:14:02 +0000

    navbar working

---
+++ Readme.md
@@ -0,0 +1,3 @@
+on nav link items the :hover effect could of been achieved using jQuery but I wanted to show that I could use css to achieve this.
+
+at the start I use bootstraps built it rows and containers for the navbar however I later build these myself to show that I can
--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,33 @@
-/* Function to make the square in the media section responsive */
-function adjustSquareHeight() {
-  let myHeight = $('section.media').height()
-  if (myHeight < 1300) {
-    myHeight *= 1.1
-  }
-  const myString = myHeight + 'px'
-  $('div.square-decoration').css('height', myString)
-  $('div.square-decoration').css('width', myString)
-}
-
 $(document).ready(() => {
+    
+  /* Change newsletter input placeholder when submitted */  
   $('button.newsletter-button').click(event => {
     event.preventDefault()
     $('input.newsletter-input').attr('placeholder', 'Subscribed')
   })
+
+  /* Logo animation on scroll */
+  $(window).scroll(function () {
+    $(".navbar-default").toggleClass("navbar-shrink", $(this).scrollTop() > 50)
+  })
+
+  /* Function to make the square in the media section responsive */
+  function adjustSquareHeight() {
+    let myHeight = $('section.media').height()
+    if (myHeight < 1300) {
+      myHeight *= 1.1
+    }
+    const myString = myHeight + 'px'
+    $('div.square-decoration').css('height', myString)
+    $('div.square-decoration').css('width', myString)
+  }
+  
+
+  /* calls adjustSquareHeight anytime the browser window is resized */
+  $(window).resize(() => {
+    adjustSquareHeight()
+  })
+
   adjustSquareHeight()
 })
 
-/* calls adjustSquareHeight anytime the browser window is resized */
-$(window).resize(() => {
-  adjustSquareHeight()
-})
