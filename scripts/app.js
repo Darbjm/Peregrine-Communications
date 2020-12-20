b99087a7a650d1584966089b12ce4fff210bc889
James Darby <56593075+Darbjm@users.noreply.github.com>
2020-12-20 14:52:44 +0000

    nav animation complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,10 @@
 $(document).ready(() => {
 
+    const scrollPercent = ($(document).scrollTop() / 100)
+
     /* Change navbar color when scrolling */
     $(window).scroll(() => {
         const scrollPercent = ($(document).scrollTop() / 100)
-
         // if user has scrolled to the top make nav clear and change logo
         if (scrollPercent < 2.5) {
             $('.logo-text').css({ fontSize: '1rem', transition: '2.0s' })
@@ -13,12 +14,21 @@ $(document).ready(() => {
             $('.nav-container').css({ paddingTop: '30px', transition: '2.0s' })
             $('.navbar').css({ backgroundColor: '#e7eeec00', transition: '2.0s' })
             $('.nav-link').css({ color: '#ffffff', transition: '2.0s' })
+            $('a.nav-link:hover').css({ color: '#f8ee9a' })
             $('.nav-border').css({ borderTop: '#ffffff 1px solid', transition: '2.0s' })
             $('.nav-border').css({ borderBottom: '#ffffff 1px solid', transition: '2.0s' })
+            $('.diamond').css({ backgroundColor: '#f8ee9a' })
+            $('a.nav-link').each(() => {
+            }).hover(function() {
+                $(this).css({
+                    color: '#f8ee9a'
+                })
+            })
         }
 
         // if user has scrolled down make nav colored and turn logo green
         if (scrollPercent > 2.5) {
+            $('.diamond').css({ backgroundColor: '#00B3AE' })
             $('.logo-text').css({ fontSize: '0', transition: '2.0s' })
             $('.path-logo').css({ fill: '#005d44', transition: '2.0s' })
             $('.svg-logo').css({ marginBottom: '0px', transition: '2.0s' })
@@ -26,14 +36,17 @@ $(document).ready(() => {
             $('.nav-container').css({ paddingTop: '15px', transition: '2.0s' })
             $('.navbar').css({ backgroundColor: '#e7eeec', transition: '2.0s' })
             $('.nav-link').css({ color: '#484848', transition: '2.0s' })
-            $('a.nav-link').hover(() => {
-                $( this ).css({ color: '#00B3AE' })
-                $('.diamond').css({ backgroundColor: '#00B3AE' })
-            })
             $('.nav-border').css({ borderTop: '#E3E3E3 1px solid', transition: '2.0s' })
             $('.nav-border').css({ borderBottom: '#ffffff00 1px solid', transition: '2.0s' })
+            $('a.nav-link').each(() => {
+            }).hover(function() {
+                $(this).css({
+                    color: '#00B3AE'
+                })
+            })
         }
     })
+
 
     /* Change newsletter input placeholder when submitted */  
     $('button.newsletter-button').click(event => {
